5cfc5867c856c57b3af1a135927ab882c9b1d515
JARED JOSE AREVALO ESPINOZA <jared.arevalo92u@std.uni.edu.ni>
2023-09-27 05:27:14 -0600

    metodo promedio

--- PlanillaDePagoContCostos/frmMetodoCpromo.Designer.cs
+++ PlanillaDePagoContCostos/frmMetodoCpromo.Designer.cs
@@ -28,18 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnAgg = new Button();
+            lblTitulo = new Label();
+            lblfecha = new Label();
+            lblValor = new Label();
+            lblCan = new Label();
+            label1 = new Label();
+            cboConcepto = new ComboBox();
+            txtCantidad = new TextBox();
+            txtValorU = new TextBox();
+            dataGridView1 = new DataGridView();
+            Fecha = new DataGridViewTextBoxColumn();
+            Concepto = new DataGridViewTextBoxColumn();
+            CantidadE = new DataGridViewTextBoxColumn();
+            ValorUnE = new DataGridViewTextBoxColumn();
+            ValorTotE = new DataGridViewTextBoxColumn();
+            CantidadS = new DataGridViewTextBoxColumn();
+            ValorUnS = new DataGridViewTextBoxColumn();
+            ValorTotS = new DataGridViewTextBoxColumn();
+            CantidadSds = new DataGridViewTextBoxColumn();
+            ValorUnSds = new DataGridViewTextBoxColumn();
+            ValorTotSds = new DataGridViewTextBoxColumn();
+            btnInicio = new Button();
+            btnBorrar = new Button();
+            btnExportar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // btnAgg
+            // 
+            btnAgg.Location = new Point(612, 136);
+            btnAgg.Name = "btnAgg";
+            btnAgg.Size = new Size(94, 29);
+            btnAgg.TabIndex = 34;
+            btnAgg.Text = "Agregar";
+            btnAgg.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lblTitulo.Location = new Point(413, 5);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(59, 23);
+            lblTitulo.TabIndex = 33;
+            lblTitulo.Text = "label4";
+            // 
+            // lblfecha
+            // 
+            lblfecha.AutoSize = true;
+            lblfecha.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblfecha.Location = new Point(94, 113);
+            lblfecha.Name = "lblfecha";
+            lblfecha.Size = new Size(65, 25);
+            lblfecha.TabIndex = 32;
+            lblfecha.Text = "label4";
+            // 
+            // lblValor
+            // 
+            lblValor.AutoSize = true;
+            lblValor.Location = new Point(413, 113);
+            lblValor.Name = "lblValor";
+            lblValor.Size = new Size(123, 20);
+            lblValor.TabIndex = 31;
+            lblValor.Text = "VALOR UNITARIO";
+            // 
+            // lblCan
+            // 
+            lblCan.AutoSize = true;
+            lblCan.Location = new Point(413, 33);
+            lblCan.Name = "lblCan";
+            lblCan.Size = new Size(83, 20);
+            lblCan.TabIndex = 30;
+            lblCan.Text = "CANTIDAD";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(94, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 20);
+            label1.TabIndex = 29;
+            label1.Text = "CONCEPTO";
+            // 
+            // cboConcepto
+            // 
+            cboConcepto.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboConcepto.FormattingEnabled = true;
+            cboConcepto.Items.AddRange(new object[] { "Venta", "Compra" });
+            cboConcepto.Location = new Point(94, 56);
+            cboConcepto.Margin = new Padding(3, 4, 3, 4);
+            cboConcepto.Name = "cboConcepto";
+            cboConcepto.Size = new Size(138, 28);
+            cboConcepto.TabIndex = 28;
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Location = new Point(413, 57);
+            txtCantidad.Margin = new Padding(3, 4, 3, 4);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(114, 27);
+            txtCantidad.TabIndex = 27;
+            // 
+            // txtValorU
+            // 
+            txtValorU.Location = new Point(413, 137);
+            txtValorU.Margin = new Padding(3, 4, 3, 4);
+            txtValorU.Name = "txtValorU";
+            txtValorU.Size = new Size(114, 27);
+            txtValorU.TabIndex = 26;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Fecha, Concepto, CantidadE, ValorUnE, ValorTotE, CantidadS, ValorUnS, ValorTotS, CantidadSds, ValorUnSds, ValorTotSds });
+            dataGridView1.Location = new Point(18, 190);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1398, 330);
+            dataGridView1.TabIndex = 25;
+            // 
+            // Fecha
+            // 
+            Fecha.HeaderText = "Fecha";
+            Fecha.MinimumWidth = 6;
+            Fecha.Name = "Fecha";
+            Fecha.ReadOnly = true;
+            Fecha.Resizable = DataGridViewTriState.True;
+            Fecha.Width = 95;
+            // 
+            // Concepto
+            // 
+            Concepto.HeaderText = "Concepto";
+            Concepto.MinimumWidth = 6;
+            Concepto.Name = "Concepto";
+            Concepto.ReadOnly = true;
+            Concepto.Width = 125;
+            // 
+            // CantidadE
+            // 
+            CantidadE.HeaderText = "Cantidad";
+            CantidadE.MinimumWidth = 6;
+            CantidadE.Name = "CantidadE";
+            CantidadE.ReadOnly = true;
+            CantidadE.Width = 125;
+            // 
+            // ValorUnE
+            // 
+            ValorUnE.HeaderText = "Valor Unitario";
+            ValorUnE.MinimumWidth = 6;
+            ValorUnE.Name = "ValorUnE";
+            ValorUnE.ReadOnly = true;
+            ValorUnE.Width = 125;
+            // 
+            // ValorTotE
+            // 
+            ValorTotE.HeaderText = "Valor Total";
+            ValorTotE.MinimumWidth = 6;
+            ValorTotE.Name = "ValorTotE";
+            ValorTotE.ReadOnly = true;
+            ValorTotE.Width = 125;
+            // 
+            // CantidadS
+            // 
+            CantidadS.HeaderText = "Cantidad";
+            CantidadS.MinimumWidth = 6;
+            CantidadS.Name = "CantidadS";
+            CantidadS.ReadOnly = true;
+            CantidadS.Width = 125;
+            // 
+            // ValorUnS
+            // 
+            ValorUnS.HeaderText = "Valor Unitario";
+            ValorUnS.MinimumWidth = 6;
+            ValorUnS.Name = "ValorUnS";
+            ValorUnS.ReadOnly = true;
+            ValorUnS.Width = 125;
+            // 
+            // ValorTotS
+            // 
+            ValorTotS.HeaderText = "Valor Total";
+            ValorTotS.MinimumWidth = 6;
+            ValorTotS.Name = "ValorTotS";
+            ValorTotS.ReadOnly = true;
+            ValorTotS.Width = 125;
+            // 
+            // CantidadSds
+            // 
+            CantidadSds.HeaderText = "Cantidad";
+            CantidadSds.MinimumWidth = 6;
+            CantidadSds.Name = "CantidadSds";
+            CantidadSds.ReadOnly = true;
+            CantidadSds.Width = 125;
+            // 
+            // ValorUnSds
+            // 
+            ValorUnSds.HeaderText = "Valor Unitario";
+            ValorUnSds.MinimumWidth = 6;
+            ValorUnSds.Name = "ValorUnSds";
+            ValorUnSds.ReadOnly = true;
+            ValorUnSds.Width = 125;
+            // 
+            // ValorTotSds
+            // 
+            ValorTotSds.HeaderText = "Valor Total";
+            ValorTotSds.MinimumWidth = 6;
+            ValorTotSds.Name = "ValorTotSds";
+            ValorTotSds.ReadOnly = true;
+            ValorTotSds.Width = 125;
+            // 
+            // btnInicio
+            // 
+            btnInicio.BackColor = Color.FromArgb(255, 224, 192);
+            btnInicio.Location = new Point(1293, 537);
+            btnInicio.Name = "btnInicio";
+            btnInicio.Size = new Size(105, 49);
+            btnInicio.TabIndex = 37;
+            btnInicio.Text = "🏠";
+            btnInicio.UseVisualStyleBackColor = false;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.BackColor = Color.FromArgb(255, 224, 192);
+            btnBorrar.Location = new Point(1170, 537);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(101, 49);
+            btnBorrar.TabIndex = 36;
+            btnBorrar.Text = "🗑";
+            btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // btnExportar
+            // 
+            btnExportar.BackColor = Color.FromArgb(255, 224, 192);
+            btnExportar.Location = new Point(12, 539);
+            btnExportar.Name = "btnExportar";
+            btnExportar.Size = new Size(146, 47);
+            btnExportar.TabIndex = 35;
+            btnExportar.Text = "EXPORTAR";
+            btnExportar.UseVisualStyleBackColor = false;
             // 
             // frmMetodoCpromo
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1424, 598);
+            Controls.Add(btnInicio);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnExportar);
+            Controls.Add(btnAgg);
+            Controls.Add(lblTitulo);
+            Controls.Add(lblfecha);
+            Controls.Add(lblValor);
+            Controls.Add(lblCan);
+            Controls.Add(label1);
+            Controls.Add(cboConcepto);
+            Controls.Add(txtCantidad);
+            Controls.Add(txtValorU);
+            Controls.Add(dataGridView1);
             Name = "frmMetodoCpromo";
             Text = "Método PROMEDIO";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnAgg;
+        private Label lblTitulo;
+        private Label lblfecha;
+        private Label lblValor;
+        private Label lblCan;
+        private Label label1;
+        private ComboBox cboConcepto;
+        private TextBox txtCantidad;
+        private TextBox txtValorU;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Fecha;
+        private DataGridViewTextBoxColumn Concepto;
+        private DataGridViewTextBoxColumn CantidadE;
+        private DataGridViewTextBoxColumn ValorUnE;
+        private DataGridViewTextBoxColumn ValorTotE;
+        private DataGridViewTextBoxColumn CantidadS;
+        private DataGridViewTextBoxColumn ValorUnS;
+        private DataGridViewTextBoxColumn ValorTotS;
+        private DataGridViewTextBoxColumn CantidadSds;
+        private DataGridViewTextBoxColumn ValorUnSds;
+        private DataGridViewTextBoxColumn ValorTotSds;
+        private Button btnInicio;
+        private Button btnBorrar;
+        private Button btnExportar;
     }
 }
--- PlanillaDePagoContCostos/frmMetodoCpromo.cs
+++ PlanillaDePagoContCostos/frmMetodoCpromo.cs
@@ -12,29 +12,319 @@ namespace PlanillaDePagoContCostos
 {
     public partial class frmMetodoCpromo : Form
     {
+        DateTime fecha = DateTime.Now;
         public frmMetodoCpromo()
         {
+
             InitializeComponent();
             // Crea el GridView
-            var gridView = new DataGridView();
-            gridView.Dock = DockStyle.Fill;
+            string fechaFormateada = fecha.ToString("dd/MM/yyyy");
+            lblfecha.Text = "Fecha: " + fechaFormateada;
+            lblTitulo.Visible = false;
+            lblCan.Visible = false;
+            lblValor.Visible = false;
 
-            // Agrega las columnas iniciales (entradas, salidas y saldos)
-            gridView.Columns.Add("Entradas", "Entradas");
-            gridView.Columns.Add("Salidas", "Salidas");
-            gridView.Columns.Add("Saldos", "Saldos");
+            // Maneja el evento de selección del ComboBox
+            cboConcepto.SelectedIndexChanged += CboConcepto_SelectedIndexChanged;
 
-            // Agrega las columnas secundarias (cantidad, valor unitario y valor total)
-            gridView.Columns.Add("Cantidad", "Cantidad");
-            gridView.Columns.Add("ValorUnitario", "Valor Unitario");
-            gridView.Columns.Add("ValorTotal", "Valor Total");
 
-            // Agrega datos de ejemplo
-            gridView.Rows.Add("10", "5", "15", "20", "10", "200", "5", "10", "50");
+            btnAgg.Visible = false;
+            btnExportar.Visible = false;
+            btnBorrar.Visible = false;
 
-            // Agrega el GridView al formulario
-            Controls.Add(gridView);
+            txtCantidad.Visible = false;
+            txtValorU.Visible = false;
+        }
 
+        private void CboConcepto_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+
+            // Obtén la opción seleccionada
+            string opcionSeleccionada = cboConcepto.SelectedItem.ToString();
+
+            // Actualiza los TextBox según la opción seleccionada
+
+            if (opcionSeleccionada == "Venta")
+            {
+                txtCantidad.Text = "";
+                txtValorU.Text = "";
+                txtCantidad.Focus();
+
+                btnAgg.Visible = true;
+                btnAgg.Enabled = false;
+                btnBorrar.Visible = true;
+                btnBorrar.Enabled = false;
+                btnExportar.Visible = true;
+
+                lblCan.Visible = true;
+                lblValor.Visible = false;
+                txtCantidad.Visible = true;
+                txtValorU.Visible = false;
+                lblTitulo.Visible = true;
+                lblTitulo.Text = "Datos de Venta:";
+            }
+            else if (opcionSeleccionada == "Compra")
+            {
+                txtCantidad.Text = "";
+                txtValorU.Text = "";
+                txtCantidad.Focus();
+
+                btnAgg.Visible = true;
+                btnAgg.Enabled = false;
+                btnBorrar.Visible = true;
+                btnBorrar.Enabled = false;
+                btnExportar.Visible = true;
+
+
+                lblCan.Visible = true;
+                lblValor.Visible = true;
+                txtCantidad.Visible = true;
+                txtValorU.Visible = true;
+                lblTitulo.Visible = true;
+                lblTitulo.Text = "Datos de Compra:";
+            }
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Bloquear caracteres no numéricos
+            }
+        }
+
+        private void txtValorU_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+                e.Handled = true;
+
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.Contains(".")))
+                e.Handled = true;
+
+            if ((e.KeyChar == '.') && (sender as System.Windows.Forms.TextBox).Text.Length == 0)
+                e.Handled = true;
+        }
+
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+            VerificarCamposNoVacios();
+
+            HabilitarBotonBorrar();
+        }
+
+        private void txtValorU_TextChanged(object sender, EventArgs e)
+        {
+            VerificarCamposNoVacios();
+
+            HabilitarBotonBorrar();
+        }
+
+
+
+
+        //Este método habilita y deshabilita los botones "Calcular" de cada figura, verificando los textbox vacíos.
+
+        private void VerificarCamposNoVacios()
+        {
+
+            if (cboConcepto.SelectedItem.ToString() == "Venta")
+            {
+                btnAgg.Enabled = !string.IsNullOrWhiteSpace(txtCantidad.Text);
+
+            }
+            else if (cboConcepto.SelectedItem.ToString() == "Compra")
+            {
+                // Si la opción seleccionada es "Compra", habilitar cuando ambos TextBox tengan contenido
+                btnAgg.Enabled = !string.IsNullOrWhiteSpace(txtCantidad.Text) && !string.IsNullOrWhiteSpace(txtValorU.Text);
+                //btnBorrar.Enabled = !string.IsNullOrWhiteSpace(txtCantidad.Text) && !string.IsNullOrWhiteSpace(txtValorU.Text);
+            }
+            else
+            {
+                btnAgg.Enabled = false;
+                // btnBorrar.Enabled = false;
+            }
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            HabilitarBotonBorrar();
+            DialogResult resultado = MessageBox.Show("¿Desea borrar los datos ingresados?", "Confirmar acción", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+            if (resultado == DialogResult.OK)
+            {
+                txtCantidad.Text = "";
+                txtValorU.Text = "";
+                txtCantidad.Focus();
+                btnBorrar.Enabled = false;
+            }
+        }
+
+        private void HabilitarBotonBorrar()
+        {
+            // Verifica si los TextBox tienen datos y habilita el botón de limpiar en consecuencia
+            btnBorrar.Enabled = !string.IsNullOrWhiteSpace(txtCantidad.Text) || !string.IsNullOrWhiteSpace(txtValorU.Text);
+        }
+
+
+
+        public void exportaraexcel(DataGridView tabla)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            // Puedes verificar si ya existe una instancia de Excel antes de crear una nueva.
+            if (excel == null)
+            {
+                MessageBox.Show("No se pudo iniciar Excel. Asegúrate de tenerlo instalado.");
+                return;
+            }
+
+            excel.Application.Workbooks.Add(true);
+
+            int IndiceColumna = 0;
+
+            foreach (DataGridViewColumn col in tabla.Columns)
+            {
+                IndiceColumna++;
+                excel.Cells[1, IndiceColumna] = col.HeaderText; // Usar HeaderText en lugar de Name
+            }
+
+            int IndeceFila = 0;
+
+            foreach (DataGridViewRow row in tabla.Rows)
+            {
+                IndeceFila++;
+                IndiceColumna = 0;
+
+                foreach (DataGridViewColumn col in tabla.Columns)
+                {
+                    IndiceColumna++;
+                    excel.Cells[IndeceFila + 1, IndiceColumna] = row.Cells[col.Index].Value;
+                }
+            }
+
+            excel.Visible = true;
+        }
+
+
+
+
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            exportaraexcel(dataGridView1);
+        }
+
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Desea regresar al menú principal?", "Confirmar acción", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.OK)
+            {
+                this.Hide();
+                frmRegistroInventario menu = new();
+                menu.ShowDialog();
+            }
+        }
+
+        private void btnAgg_Click_1(object sender, EventArgs e)
+        {
+            // Obtén la opción seleccionada en el ComboBox
+            string opcionSeleccionada = cboConcepto.SelectedItem.ToString();
+            double Pventa = CalcularPrecioVenta();
+            double total = double.Parse(txtCantidad.Text) * double.Parse(txtValorU.Text);
+            double totalV = Pventa * double.Parse(txtCantidad.Text);
+            decimal saldo = totalDebe() - totalHaber();
+
+            // Obtén la fecha actual formateada
+            string fechaFormateada = fecha.ToString("dd/MM/yyyy");
+
+            // Agrega una nueva fila al DataGridView
+            int rowIndex = dataGridView1.Rows.Add();
+
+            // Establece la fecha en la primera columna
+            dataGridView1.Rows[rowIndex].Cells[0].Value = fechaFormateada;
+
+            // Establece la opción seleccionada en la segunda columna
+            dataGridView1.Rows[rowIndex].Cells[1].Value = opcionSeleccionada;
+
+            // Verifica la opción y agrega los datos correspondientes en las columnas restantes
+            if (opcionSeleccionada == "Venta")
+            {
+                // Agrega la cantidad en la sexta columna
+                dataGridView1.Rows[rowIndex].Cells[5].Value = txtCantidad.Text;
+                dataGridView1.Rows[rowIndex].Cells[6].Value = Pventa;
+                dataGridView1.Rows[rowIndex].Cells[7].Value = totalV;
+                dataGridView1.Rows[rowIndex].Cells[10].Value = totalV;
+            }
+            else if (opcionSeleccionada == "Compra")
+            {
+                // Agrega el valor unitario en la tercera columna y la cantidad en la segunda columna
+                dataGridView1.Rows[rowIndex].Cells[2].Value = txtCantidad.Text;// Si deseas poner cantidad en la columna 2
+                dataGridView1.Rows[rowIndex].Cells[3].Value = txtValorU.Text;
+                dataGridView1.Rows[rowIndex].Cells[4].Value = total;
+                dataGridView1.Rows[rowIndex].Cells[9].Value = total;
+            }
+            dataGridView1.Rows[rowIndex].Cells[11].Value = saldo.ToString();
+
+            //dataGridView1.Rows[rowIndex].Cells[11].Value = ;
+            // Limpia los TextBox después de agregar los datos
+            txtCantidad.Text = "";
+            txtValorU.Text = "";
+
+            // Deshabilita el botón "Agregar"
+            btnAgg.Enabled = false;
+
+            // Vuelve a verificar si debe habilitar el botón "Borrar"
+            HabilitarBotonBorrar();
+        }
+
+        private decimal totalHaber()
+        {
+            decimal Total = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                Total = Convert.ToDecimal(row.Cells["Cantidads"].Value);
+            }
+            return Total;
+        }
+
+        private decimal totalDebe()
+        {
+            decimal Total = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                Total = Convert.ToDecimal(row.Cells["CantidadSds"].Value);
+            }
+            return Total;
+        }
+
+        private decimal totalProductoSal()
+        {
+            decimal Total = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                Total = Convert.ToDecimal(row.Cells["ValorUnSds"].Value);
+            }
+            return Total;
+        }
+
+        private decimal totalProductoEnt()
+        {
+            decimal Total = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                Total = Convert.ToDecimal(row.Cells["CantidadE"].Value);
+            }
+            return Total;
+        }
+
+        private double CalcularPrecioVenta()
+        {
+            decimal PrecioVenta = 0;
+            PrecioVenta = (totalDebe() - totalHaber()) * totalProductoEnt() - totalProductoSal();
+            return Convert.ToDouble(PrecioVenta);
         }
     }
 }
